0f169a6d379846bf9b1055268546ded391914b58
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-04-12 12:36:48 -0500

    Basic CSS completed. Adding useState

--- expanding-card/src/App.js
+++ expanding-card/src/App.js
@@ -2,6 +2,8 @@ import {motion} from 'framer-motion';
 import { useState } from 'react';
 
 function App() {
+
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="App">
       <motion.div className="card">
