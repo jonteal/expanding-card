095c9ce0949513e9fff4f694728cb5c4e3a65677
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-04-12 12:47:11 -0500

    Added width to expanded card to make it smaller.

--- expanding-card/src/App.js
+++ expanding-card/src/App.js
@@ -6,12 +6,13 @@ function App() {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="App">
-      <motion.div layout onClick={() => setIsOpen(!isOpen)} className="card">
+      <motion.div transition={{layout: {duration: 1, type: "spring"}}} layout onClick={() => setIsOpen(!isOpen)} className="card">
         <motion.h2 layout="position">Framer Motion 🚀</motion.h2>
         {isOpen && (
-        <motion.div>
+        <motion.div className='expand'>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, dolorum obcaecati sunt nulla molestiae rem ratione veniam odit dignissimos? Quisquam.</p>
           <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat, tempora?</p>
+          <button>Hello</button>
         </motion.div>
 
         )}
