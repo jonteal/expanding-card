c9c569578fbf4b3cdbd51ae5c0ec1a030547cab9
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-04-12 12:30:11 -0500

    Initial commit

---
+++ expanding-card/src/App.js
@@ -0,0 +1,14 @@
+import {motion} from 'framer-motion';
+import { useState } from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <motion.div className="card">
+
+      </motion.div>
+    </div>
+  );
+}
+
+export default App;
